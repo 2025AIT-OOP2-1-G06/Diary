057307d7ca82f7a52a5107ebd89bd872e98ee963
ganbaruru <takuto2234@icloud.com>
2025-10-16 15:18:46 +0900

    ganbaruruDiary-add

---
+++ diaries/ganbaruruDiary.py
@@ -0,0 +1,9 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class ganbaruruDiary(AbstractDiary):
+	def get_date(self):
+		return "2025-10-16"
+	def get_summary(self):
+		return "Git<strike>難しすぎる"
+	def get_author(self):
+		return "ganbaruru"
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from diaries.DiarySample import DiarySample
+from diaries.ganbaruruDiary import ganbaruruDiary
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
 	DiarySample(), 
+	ganbaruruDiary(),
 ]
 
 for d in diaries:
