ef1293756fd8d2e50d72947151bfa9b14dee2191
中島朝登 <asato624@icloud.com>
2025-10-16 16:14:38 +0900

    hennkou-02

---
+++ diaries/JinmaDiary.py
@@ -0,0 +1,9 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class JinmaDiary(AbstractDiary):
+	def get_date(self):
+		return "2025-10-16"
+	def get_summary(self):
+		return "なんとかできた"
+	def get_author(self):
+		return "Jinma(nakashima)"
